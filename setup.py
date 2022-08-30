fcb87c8c48aa61f4e7bc1eec6d971b358128829b
vorpal56 <anthonysukadil1@gmail.com>
2022-08-30 17:56:05 -0400

    Bump websockets due to Python 3.7 issue, upgrade requests and PureCloud without any issues

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from setuptools import setup
 
 setup(name='tap-purecloud',
-      version='0.0.8',
+      version='0.0.9',
       description='Singer.io tap for extracting data from the Genesys Purecloud API',
       author='Fishtown Analytics',
       url='http://fishtownanalytics.com',
@@ -12,10 +12,12 @@ setup(name='tap-purecloud',
       install_requires=[
           'singer-python==5.12.0',
           'backoff==1.8.0',
-          'requests==2.20.0',
+          'requests==2.25.1',
           'python-dateutil==2.6.0',
-          'PureCloudPlatformClientV2==64.0.1',
-          'websockets==5.0.1'
+          'PureCloudPlatformClientV2==151.0.0',
+          # Using Python 3.7 so upgrade from 5.0.1 to 6.0.0
+          # See https://github.com/aaugustin/websockets/issues/432
+          'websockets==6.0.0'
       ],
       entry_points='''
           [console_scripts]
